9551a803d7d76170ae69e4dd305c75c709f0283c
austin <jamies4real2016@gmail.com>
2020-10-18 18:02:30 +0100

    Updates

---
+++ precache-manifest.556669d02666c6f066d8935eb06c7e1c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9dd7cfd79b944c1441d440b1ade912bd",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "80f9ab4b71c461be7911",
+    "url": "/react-gh-pages/static/css/main.01ea3825.chunk.css"
+  },
+  {
+    "revision": "bf872a583732d10eecad",
+    "url": "/react-gh-pages/static/js/2.234e5370.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-gh-pages/static/js/2.234e5370.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80f9ab4b71c461be7911",
+    "url": "/react-gh-pages/static/js/main.01c5edd9.chunk.js"
+  },
+  {
+    "revision": "60c3baa1bba0463e1f20",
+    "url": "/react-gh-pages/static/js/runtime-main.d5c43e5d.js"
+  }
+]);
